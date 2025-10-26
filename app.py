cdbd52ba09daf262e5afc8c9c6ce65dc50674149
Reza Ghasemi <rezaghasemi@Rezas-MacBook-Air.local>
2025-10-26 17:49:58 -0600

    working on backend using FastAPI

--- app.py
+++ app.py
@@ -1,13 +1,41 @@
-from src.db.dbHandler import add_feedback, get_all_feedback
+from src.db.dbHandler import add_feedback, add_generated_content
 from omegaconf import OmegaConf
-from src.models.txt_model import generate_hashtags
-from src.models.fig_model import generate_image
+# from src.models.txt_model import generate_hashtags
+# from src.models.fig_model import generate_image
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+
+
 
 cfg = OmegaConf.load("src/configs/configs.yaml")
 
+class Prompt(BaseModel):
+    description: str
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Hashtag and Image Generation API!"}
 
 
+@app.post("/generate/")
+async def generate_content(prompt: Prompt):
+    description = prompt.description
+    try:
+        hashtags = generate_hashtags(description, cfg)
+        img = generate_image(description, cfg)
 
+
+        return {
+            "hashtags": hashtags,
+            "image_url": image_url
+        }
+        add_feedback(description, hashtags, image_url)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Generation failed")
 
 
 
--- src/db/dbHandler.py
+++ src/db/dbHandler.py
@@ -7,8 +7,8 @@ Base = declarative_base()
 cfg = OmegaConf.load("src/configs/configs.yaml")
 
 
-class HashtagFeedback(Base):
-    __tablename__ = "hashtag_feedback"
+class genContent(Base):
+    __tablename__ = "generated_content"
     id = Column(Integer, primary_key=True)
     text = Column(Text)
     hashtags = Column(Text)
@@ -21,15 +21,29 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-def add_feedback(text, hashtags, user_feedback):
-    feedback = HashtagFeedback(
-        text=text,
-        hashtags=hashtags,
-        user_feedback=user_feedback
+def add_feedback(id, user_feedback = 0):
+    # Fetch the record
+    feedback = session.query(genContent).filter(genContent.id == id).first()
+    # if record exists
+    if feedback:
+        # Update the field
+        feedback.user_feedback = user_feedback
+        # Commit the change
+        session.commit()
+        return feedback
+    return None
+
+
+def add_generated_content(description, hashtags, image_url):
+    feedback = genContent(
+        text=description,
+        hashtags=",".join(hashtags),
+        user_feedback=0
     )
     session.add(feedback)
     session.commit()
-    return feedback.id
+    return feedback.id  
+
 
 def get_all_feedback():
-    return session.query(HashtagFeedback).all()
+    return session.query(genContent).all()
