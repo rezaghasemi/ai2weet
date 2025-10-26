fd113f19aa818131aecf955e08c73ae38498cff6
Reza Ghasemi <rezaghasemi@Rezas-MacBook-Air.local>
2025-10-26 04:12:12 -0600

    Working on downloading and storing models on the local computer.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from src.db.dbHandler import add_feedback, get_all_feedback
 from omegaconf import OmegaConf
 from src.models.txt_model import generate_hashtags
-
+from src.models.fig_model import generate_image
 
 cfg = OmegaConf.load("src/configs/configs.yaml")
 
